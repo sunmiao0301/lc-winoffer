8fb2400ed85ea5c89b56d32fafbe205001ea9a4e
sunmiao0301 <52996768+sunmiao0301@users.noreply.github.com>
2021-08-24 23:32:44 +0800

    Create 03、栈与队列：用队列实现栈还有点别扭 - 225. 用队列实现栈.java
    请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
    
    实现 MyStack 类：
    
    void push(int x) 将元素 x 压入栈顶。
    int pop() 移除并返回栈顶元素。
    int top() 返回栈顶元素。
    boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
     
    
    注意：
    
    你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
    你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
     
    
    示例：
    
    输入：
    ["MyStack", "push", "push", "top", "pop", "empty"]
    [[], [1], [2], [], [], []]
    输出：
    [null, null, null, 2, 2, false]
    
    解释：
    MyStack myStack = new MyStack();
    myStack.push(1);
    myStack.push(2);
    myStack.top(); // 返回 2
    myStack.pop(); // 返回 2
    myStack.empty(); // 返回 False
     
    
    提示：
    
    1 <= x <= 9
    最多调用100 次 push、pop、top 和 empty
    每次调用 pop 和 top 都保证栈不为空
     
    
    进阶：你能否实现每种操作的均摊时间复杂度为 O(1) 的栈？换句话说，执行 n 个操作的总时间复杂度 O(n) ，尽管其中某个操作可能需要比其他操作更长的时间。你可以使用两个以上的队列。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/implement-stack-using-queues
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ LeetCode_按标签刷/06、栈与队列/03、栈与队列：用队列实现栈还有点别扭 - 225. 用队列实现栈.java
@@ -0,0 +1,116 @@
+//第一版 先做只用两个Queue完成的
+    1)
+    请你仅使用两个队列实现一个后入先出（LIFO）的栈，
+    并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+    因为size()卡了一下 但是自己解决了
+
+出错的原因是size()函数需要注意！
+包括未来的String.length()都要注意 
+防止被动体变化导致for循环中的size()值变化，
+导致for循环的结果与预想的值不一致
+
+class MyStack {
+    Queue<Integer> queueIn;
+    Queue<Integer> queueOut;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queueIn = new LinkedList<>();
+        queueOut = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        if(!queueOut.isEmpty())
+            queueOut.offer(x);
+        else
+            queueIn.offer(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        if(queueIn.isEmpty()){
+            int size = queueOut.size();//····························如果这里不先取出来size值，for循环的时候 queue.size()会随着循环而变小，进而和想要的效果不一致，出错
+            for(int i = 0; i < size - 1; i++){
+                queueIn.offer(queueOut.poll());
+            }
+            return queueOut.poll();
+        }
+        else{
+            int size = queueIn.size();
+            for(int i = 0; i < size - 1; i++){
+                queueOut.offer(queueIn.poll());
+            }
+            return queueIn.poll();
+        }
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        if(queueIn.isEmpty()){
+            int size = queueOut.size();
+            for(int i = 0; i < size - 1; i++){
+                queueIn.offer(queueOut.poll());
+            }
+            int temp = queueOut.poll();
+            queueIn.offer(temp);
+            return temp;
+        }
+        else{
+            int size = queueIn.size();
+            for(int i = 0; i < size - 1; i++){
+                queueOut.offer(queueIn.poll());
+            }
+            int temp = queueIn.poll();
+            queueOut.offer(temp);
+            return temp;
+        }        
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queueIn.isEmpty() && queueOut.isEmpty();
+    }
+}
+
+//第二版
+    2)
+    进阶：你能否实现每种操作的均摊时间复杂度为 O(1) 的栈？
+    换句话说，执行 n 个操作的总时间复杂度 O(n) ，
+    尽管其中某个操作可能需要比其他操作更长的时间。
+    你可以使用两个以上的队列。
+    
+想半天没想出来，实际上是： 
+
+方法一使用了两个队列实现栈的操作，也可以使用一个队列实现栈的操作。（如果用两个，其实与一个没有区别，queue2其实一直都属于空的状态，就是为了放进去一个新元素，然后把queue1的元素折腾进去，使得后放进去的元素在最前面，然后再交换queue1和queue2，这时候queue2就又为空了。）
+使用一个队列时，为了满足栈的特性，即最后入栈的元素最先出栈，同样需要满足队列前端的元素是最后入栈的元素。
+入栈操作时，首先获得入栈前的元素个数 nn，然后将元素入队到队列，再将队列中的前 nn 个元素（即除了新入栈的元素之外的全部元素）依次出队并入队到队列，此时队列的前端的元素即为新入栈的元素，且队列的前端和后端分别对应栈顶和栈底。
+由于每次入栈操作都确保队列的前端元素为栈顶元素，因此出栈操作和获得栈顶元素操作都可以简单实现。出栈操作只需要移除队列的前端元素并返回即可，获得栈顶元素操作只需要获得队列的前端元素并返回即可（不移除元素）。
+由于队列用于存储栈内的元素，判断栈是否为空时，只需要判断队列是否为空即可。
+
+class MyStack {
+    Queue<Integer> queue;
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {//这里是最关键的，通过一个int值存储queue中当前元素数，然后按照stack的逻辑全部取出再放回。不懂的话看链接：https://leetcode-cn.com/problems/implement-stack-using-queues/solution/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/
+        int size = queue.size();
+        queue.offer(x);
+        while(size > 0){
+            queue.offer(queue.poll());
+            size--;
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
