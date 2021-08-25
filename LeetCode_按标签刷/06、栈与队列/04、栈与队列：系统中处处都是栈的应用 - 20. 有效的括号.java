85721041b2a95db8a83be4394c79cb8cef79e938
sunmiao0301 <52996768+sunmiao0301@users.noreply.github.com>
2021-08-25 13:18:17 +0800

    Create 04、栈与队列：系统中处处都是栈的应用 - 20. 有效的括号.java
    给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
    
    有效字符串需满足：
    
    左括号必须用相同类型的右括号闭合。
    左括号必须以正确的顺序闭合。
     
    
    示例 1：
    
    输入：s = "()"
    输出：true
    示例 2：
    
    输入：s = "()[]{}"
    输出：true
    示例 3：
    
    输入：s = "(]"
    输出：false
    示例 4：
    
    输入：s = "([)]"
    输出：false
    示例 5：
    
    输入：s = "{[]}"
    输出：true
     
    
    提示：
    
    1 <= s.length <= 104
    s 仅由括号 '()[]{}' 组成
    通过次数741,962提交次数1,668,843
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/valid-parentheses
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ LeetCode_按标签刷/06、栈与队列/04、栈与队列：系统中处处都是栈的应用 - 20. 有效的括号.java
@@ -0,0 +1,90 @@
+//第一版 一遍过 但是效率一般 唯一需要注意的是 交 并 之间的括号需要注意 也就是：
+((s.charAt(i) == ')' && stack.peek() == '(') || (s.charAt(i) == '}' && stack.peek() == '{') || (s.charAt(i) == ']' && stack.peek() == '['))
+需要一个大括号括住，不然后面的 || 与前面的(!stack.isEmpty())就构成了并 即使第一个不满足 后面的满足 依然执行 这样与需要的逻辑不符
+
+执行结果：
+通过
+执行用时：
+2 ms
+, 在所有 Java 提交中击败了
+68.17%
+的用户
+内存消耗：
+36.7 MB
+, 在所有 Java 提交中击败了
+23.46%
+的用户 
+
+//这一题与我之前笔试时遇到的一道题很相像，那个题是箱子装砖头的问题好像是，待会找找看
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            //1 <= s.length <= 104
+            if(!stack.isEmpty()
+            && ((s.charAt(i) == ')' && stack.peek() == '(')
+            || (s.charAt(i) == '}' && stack.peek() == '{')
+            || (s.charAt(i) == ']' && stack.peek() == '[')
+            ))
+            {
+                stack.pop();
+            }
+            else  
+                stack.push(s.charAt(i));
+        }
+        return stack.isEmpty();
+    }
+}
+
+//第二版 路人版 很牛 这个思路的精髓在于 “预判” 并且化 “字符间的对应” 为 “本质上的对应” 很巧妙
+一旦有[ 就立刻在stack放入] 因为必然是这样的，只有]之前的所有对称都结束了，才能轮到验证]与[
+也就是吧 [()] 化成等价的[]()
+这样就不需要根据s.charAt(i)来判断 而直接stack.pop()看是否相等即可。
+
+执行结果：
+通过
+执行用时：
+1 ms
+, 在所有 Java 提交中击败了
+99.18%
+的用户
+内存消耗：
+36.7 MB
+, 在所有 Java 提交中击败了
+19.19%
+的用户
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            if(s.charAt(i) == '[')
+                stack.push(']');
+            else if(s.charAt(i) == '{')
+                stack.push('}');
+            else if(s.charAt(i) == '(')
+                stack.push(')');
+            else if(stack.isEmpty() || s.charAt(i) != stack.pop())//·······················需要注意的是 这里即使是判断一下 也pop执行了
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
+
+//第三版 K神 代码可复用性强 但是空间需要（hashmap）
+class Solution {
+    private static final Map<Character,Character> map = new HashMap<Character,Character>(){{
+        put('{','}'); put('[',']'); put('(',')'); put('?','?');
+    }};
+    public boolean isValid(String s) {
+        if(s.length() > 0 && !map.containsKey(s.charAt(0))) return false;
+        LinkedList<Character> stack = new LinkedList<Character>() {{ add('?'); }};
+        for(Character c : s.toCharArray()){
+            if(map.containsKey(c)) stack.addLast(c);
+            else if(map.get(stack.removeLast()) != c) return false;
+        }
+        return stack.size() == 1;
+    }
+}
