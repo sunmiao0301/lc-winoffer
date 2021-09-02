9956fde9f40bf1fcaf474401e20c12152576daee
sunmiao0301 <52996768+sunmiao0301@users.noreply.github.com>
2021-09-03 01:23:25 +0800

    Update and rename 03、二叉树：二叉树的非递归遍历 - 深度优先遍历 - 144. 二叉树的前序遍历 - 94. 二叉树的中序遍历 - 145. 二叉树的后序遍历.java to 03、二叉树：二叉树的非递归遍历 - 深度优先遍历 - 144. 二叉树的前序遍历 - 94. 二叉树的中序遍历(略有难度) - 145. 二叉树的后序遍历.java
    总结
    此时我们用迭代法写出了二叉树的前后中序遍历，大家可以看出前序和中序是完全两种代码风格，并不想递归写法那样代码稍做调整，就可以实现前后中序。
    
    这是因为前序遍历中访问节点（遍历节点）和处理节点（将元素放进result数组中）可以同步处理，但是中序就无法做到同步！
    
    上面这句话，可能一些同学不太理解，建议自己亲手用迭代法，先写出来前序，再试试能不能写出中序，就能理解了。
    
    那么问题又来了，难道 二叉树前后中序遍历的迭代法实现，就不能风格统一么（即前序遍历 改变代码顺序就可以实现中序 和 后序）？
    
    当然可以，这种写法，还不是很好理解，我们将在下一篇文章里重点讲解，敬请期待！

---
+++ LeetCode_按标签刷/07、二叉树/03、二叉树：二叉树的非递归遍历 - 深度优先遍历 - 144. 二叉树的前序遍历 - 94. 二叉树的中序遍历(略有难度) - 145. 二叉树的后序遍历.java
@@ -0,0 +1,124 @@
+/*
+首先还是二叉树的定义来一遍 
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//然后开始非递归法写三种深度优先遍历
+//辅助栈 先序遍历 根左右 还是根据力扣里面的要求来吧 - LeetCode 144
+执行结果：
+通过
+执行用时：
+0 ms
+, 在所有 Java 提交中击败了
+100.00%
+的用户
+内存消耗：
+36.8 MB
+, 在所有 Java 提交中击败了
+34.35%
+的用户
+通过测试用例：
+69 / 69
+class Solution {
+    //根左右
+    public List<Integer> preorderTraversal(TreeNode root) {
+    Stack<TreeNode> stack = new Stack<>();
+    List<Integer> ret = new ArrayList<>();
+    if(root == null)
+        return ret;
+    stack.push(root);
+    while(!stack.isEmpty()){
+        TreeNode temp = stack.pop();
+        if(temp.right != null)
+            stack.push(temp.right);
+        if(temp.left != null)
+            stack.push(temp.left);
+        ret.add(temp.val);
+    }
+    return ret;
+    }
+}
+
+//辅助栈 后序遍历 左右根 还是根据力扣里面的要求来吧 - LeetCode 145
+执行结果：
+通过
+执行用时：
+0 ms
+, 在所有 Java 提交中击败了
+100.00%
+的用户
+内存消耗：
+36.7 MB
+, 在所有 Java 提交中击败了
+48.77%
+的用户
+通过测试用例：
+68 / 68
+class Solution {
+    //首先分析一下后序遍历与先序遍历的区别在于？ 区别不大 思路是完全一致的 放入的顺序问题 非递归法深度遍历的难度在于中序遍历
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        /*
+        这里需要注意一下，如果是只用add，offer等函数，直接list<> list = new LinkedList/ArrayList<>();即可
+        但是如果要用addFirst,offerFirst等函数，还是LinkedList<> list = new LinkedList/ArrayList<>();才行
+        综上，LinkedList<Integer> list = new LinkedList/ArrayList<>();是最为稳妥的
+        */
+        Stack<TreeNode> stack = new Stack<>();
+        if(root == null)return list;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+            list.offerFirst(temp.val);
+            if(temp.left != null)
+                stack.push(temp.left);
+            if(temp.right != null)
+                stack.push(temp.right);
+        }
+        return list;
+    }
+}
+
+//辅助栈 中序遍历 左根右 还是根据力扣里面的要求来吧 - LeetCode 94
+这一题中序遍历非递归法思考的时候感觉用迭代法还是有点难度的，主要在于只能用栈。
+非递归法深度遍历的难度在于中序遍历
+
+没转过来 就是想不到 下面是标准题解之一
+实际上迭代法的非中序遍历的核心在于一句话：
+递归的调用过程是不断往左边走，当左边走不下去了，就打印节点，并转向右边，然后右边继续这个过程。
+只要理解了中序遍历的 左 根 右 的核心是这句话 就不难实现了
+
+class Solution {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		while(stack.size()>0 || root!=null) {
+			//不断往左子树方向走，每走一次就将当前节点保存到栈中
+			//这是模拟递归的调用
+			if(root!=null) {
+				stack.add(root);
+				root = root.left;
+			//当前节点为空，说明左边走到头了，从栈中弹出节点并保存
+			//然后转向右边节点，继续上面整个过程
+			} else {
+				TreeNode tmp = stack.pop();
+				res.add(tmp.val);
+				root = tmp.right;
+			}
+		}
+		return res;
+	}
+}
