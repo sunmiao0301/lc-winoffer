1b50c580ecde53b5323aa9df32a26b4c10407318
Miao Sun <hfut_sm@163.com>
2022-03-20 23:55:52 +0800

    Update 33、二叉树：构造一棵搜索树 - 108. 将有序数组转换为二叉搜索树.java

--- LeetCodeForkedFromLCmaster/07、二叉树/33、二叉树：构造一棵搜索树 - 108. 将有序数组转换为二叉搜索树.java
+++ LeetCodeForkedFromLCmaster/07、二叉树/33、二叉树：构造一棵搜索树 - 108. 将有序数组转换为二叉搜索树.java
@@ -1,3 +1,43 @@
+## 二刷 - 下面是我二刷写的第一版 但是有错：如A处，root.left的值拿到是在helper函数的最后，而在这之前，helper函数就需要一个root.left值，这时候root.left还是空，如此就会报错。
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, root.left, left, mid - 1); ----------------A
+        root.right = helper(nums, root.right, left, mid + 1);
+        return root;
+    }
+    public TreeNode helper(int[] nums, TreeNode root, int left, int right){
+        int mid = left + right;
+        root.left = helper(nums, root.left, left, mid - 1);
+        root.right = helper(nums, root.right, left, mid + 1);
+        return new TreeNode(nums[mid]);
+    }
+}
+
+## 二刷 正解（但是我没写出来）
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
+
 第一版 错误的
 原因是我记错了浮点型强制转换整型是向下取整而不是向上取整的！
 class Solution {
