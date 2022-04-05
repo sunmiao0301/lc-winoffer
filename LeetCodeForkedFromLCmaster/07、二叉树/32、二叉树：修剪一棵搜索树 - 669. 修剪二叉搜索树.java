848c77017c937743de1133f5859d7ff57276c299
Miao Sun <hfut_sm@163.com>
2022-04-05 16:26:45 +0800

    Update 32、二叉树：修剪一棵搜索树 - 669. 修剪二叉搜索树.java

--- LeetCodeForkedFromLCmaster/07、二叉树/32、二叉树：修剪一棵搜索树 - 669. 修剪二叉搜索树.java
+++ LeetCodeForkedFromLCmaster/07、二叉树/32、二叉树：修剪一棵搜索树 - 669. 修剪二叉搜索树.java
@@ -1,3 +1,9 @@
+## 二刷 看了题解之后：还是分为迭代法和递归法两种来写：
+-- 迭代法思路，在剪枝的时候，可以分为三步：
+1）将root移动到[L, R] 范围内，注意是左闭右闭区间
+2）剪枝左子树 --- 所谓的修建也是要有思路的 不是很简单的
+3）剪枝右子树
+
 ## 二刷 第一版 不对 
 比如对于
 [3,0,4,null,2,null,null,1]
@@ -111,62 +117,8 @@ class Solution {
     }
 }
 
-## 二刷 第三版 还是不对 --- 不想再改了 成面向样例编程了
-class Solution {
-    public TreeNode trimBST(TreeNode root, int low, int high) {
-        //可以证明，存在 唯一的答案 。
-        //修剪树 不应该 改变保留在树中的元素的相对结构
-        //可以由两个函数分别进行对上下界的剪枝
-        //与root的关系应该需要单独判断一下 --> 用于确定一个新的root -- 保证root在上下界[==]之间，然后再操作trimMin和trimMax
-        while(root.val < low || root.val > high){
-            if(root.val < low && root.right != null){
-                root = root.right;
-            }
-            else if(root.val < low && root.right == null){
-                return null;
-            }
-            else if(root.val > high && root.left != null){
-                root = root.left;
-            }
-            else if(root.val > high && root.left == null){
-                return null;
-            }
-        }
-        trimMin(root, low);
-        trimMax(root, high);
-        return root;
-    }
-    public void trimMin(TreeNode root, int low){
-        if(root.left != null && root.left.val >= low){//root.val >= low && 
-            trimMin(root.left, low);
-        }
-        else if(root.left != null && root.left.val < low){
-            if(root.left.right != null && root.left.right.val >= low)
-                root.left = root.left.right;
-            else
-                root.left = null;
-        }
-        else{//root.left == null
-            return ;
-        }
-    }
-    public void trimMax(TreeNode root, int high){
-        if(root.right != null && root.right.val <= high){//root.val >= low && 
-            trimMax(root.right, high);
-        }
-        else if(root.right != null && root.right.val > high){
-            if(root.right.left != null && root.right.left.val <= high)
-                root.right = root.right.left;
-            else
-                root.right = null;
-        }
-        else{//root.right == null
-            return ;
-        }
-    }
-}
+#### 到这里能感觉到迭代法还是有点啰嗦了 还是写递归吧
 
-#### 二刷 第四版 用非helper()函数的格式写的
 
 
 第一版 自从在上一题中解决了pointer问题，现在递归写的漂亮多了 但是这一版还不对
