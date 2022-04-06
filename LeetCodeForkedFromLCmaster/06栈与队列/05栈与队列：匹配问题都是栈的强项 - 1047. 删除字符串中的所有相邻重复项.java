ee63b8c573350170323cfd114e7246f7b792be56
Miao Sun <hfut_sm@163.com>
2022-04-06 19:35:51 +0800

    Update 05栈与队列：匹配问题都是栈的强项 - 1047. 删除字符串中的所有相邻重复项.java

--- LeetCodeForkedFromLCmaster/06栈与队列/05栈与队列：匹配问题都是栈的强项 - 1047. 删除字符串中的所有相邻重复项.java
+++ LeetCodeForkedFromLCmaster/06栈与队列/05栈与队列：匹配问题都是栈的强项 - 1047. 删除字符串中的所有相邻重复项.java
@@ -72,17 +72,20 @@ class Solution {
 
 ## 二刷 题解 StringBuilder 不一定非要用栈 而是用到栈的思想
 class Solution {
-    public String removeDuplicates(String S) {
-        char[] s = S.toCharArray();
+    public String removeDuplicates(String s) {
+        StringBuffer stack = new StringBuffer();
         int top = -1;
-        for (int i = 0; i < S.length(); i++) {
-            if (top == -1 || s[top] != s[i]) {
-                s[++top] = s[i];
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (top >= 0 && stack.charAt(top) == ch) {
+                stack.deleteCharAt(top);
+                --top;
             } else {
-                top--;
+                stack.append(ch);
+                ++top;
             }
         }
-        return String.valueOf(s, 0, top + 1);
+        return stack.toString();
     }
 }
 
