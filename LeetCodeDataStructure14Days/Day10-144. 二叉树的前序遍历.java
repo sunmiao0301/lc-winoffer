dbd96a21d354c4e338ac52c35c560b5f283b8af2
Miao Sun <hfut_sm@163.com>
2022-04-08 00:27:12 +0800

    Update Day10-144. 二叉树的前序遍历.java

--- LeetCodeDataStructure14Days/Day10-144. 二叉树的前序遍历.java
+++ LeetCodeDataStructure14Days/Day10-144. 二叉树的前序遍历.java
@@ -38,6 +38,23 @@ class Solution {
 }
 
 ## 第一版 迭代
+执行结果：
+通过
+显示详情
+添加备注
+
+执行用时：
+0 ms
+, 在所有 Java 提交中击败了
+100.00%
+的用户
+内存消耗：
+39.8 MB
+, 在所有 Java 提交中击败了
+9.72%
+的用户
+通过测试用例：
+69 / 69
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         //递归 = 栈
