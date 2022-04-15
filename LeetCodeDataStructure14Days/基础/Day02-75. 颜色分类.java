066896dda53c30f788362ea1902e3e63d45dabd6
Miao Sun <hfut_sm@163.com>
2022-04-15 14:06:23 +0800

    Update Day02-75. 颜色分类.java

--- LeetCodeDataStructure14Days/基础/Day02-75. 颜色分类.java
+++ LeetCodeDataStructure14Days/基础/Day02-75. 颜色分类.java
@@ -11,3 +11,91 @@ class Solution {
 }
 
 ## 题解 本题是经典的「荷兰国旗问题」，由计算机科学家 Edsger W. Dijkstra 首先提出
+
+## 第二版 首先先实现了 但是用的是单指针 并且遍历了不止单趟
+
+执行结果：
+通过
+显示详情
+添加备注
+
+执行用时：
+0 ms
+, 在所有 Java 提交中击败了
+100.00%
+的用户
+内存消耗：
+40 MB
+, 在所有 Java 提交中击败了
+13.94%
+的用户
+通过测试用例：
+87 / 87
+
+class Solution {
+    public void sortColors(int[] nums) {
+        //单指针
+        int noZero = 0;
+        int i = nums.length - 1;
+        while(noZero < i){
+            while(noZero < i && nums[noZero] == 0){
+                noZero++;
+            }
+            while(noZero < i && nums[i] != 0){
+                i--;
+            }
+            if(noZero >= i){
+                break;
+            }
+            swap(noZero, i, nums);
+        }
+        int noOne = noZero;
+        i = nums.length - 1;
+
+        while(noOne < i){
+            while(noOne < i && nums[noOne] == 1){
+                noOne++;
+            }
+            while(noOne < i && nums[i] != 1){
+                i--;
+            }
+            if(noOne >= i){
+                break;
+            }
+            swap(noOne, i, nums);
+        }
+
+    }
+    public void swap(int a, int b, int[] arr){
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
+
+p.s. 单指针的题解
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+    }
+}
+
+## 第二版 用双指针 单趟解决
