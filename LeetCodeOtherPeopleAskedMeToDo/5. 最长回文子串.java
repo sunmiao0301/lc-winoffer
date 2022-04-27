b69219ee2825d2ed2782162c827ee767525c1101
Miao Sun <hfut_sm@163.com>
2022-04-27 23:26:52 +0800

    Update 5. 最长回文子串.java

--- LeetCodeOtherPeopleAskedMeToDo/5. 最长回文子串.java
+++ LeetCodeOtherPeopleAskedMeToDo/5. 最长回文子串.java
@@ -1,4 +1,4 @@
-## 看题解 但是没看代码 然后自己写出来的 但是速度却很一般
+## 看题解 但是没看代码 然后自己写出来的 但是速度很一般
 
 执行结果：
 通过
@@ -63,5 +63,63 @@ class Solution {
         }
     }
     return s.substring(left, right + 1);
+    }
+}
+
+## 力扣官方题解的速度也很慢 所以这里摘录一个评论区看到的速度很快的
+
+执行结果：
+通过
+显示详情
+添加备注
+
+执行用时：
+2 ms
+, 在所有 Java 提交中击败了
+99.84%
+的用户
+内存消耗：
+41.1 MB
+, 在所有 Java 提交中击败了
+88.50%
+的用户
+通过测试用例：
+180 / 180
+
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+//         保存起始位置，测试了用数组似乎能比全局变量稍快一点
+        int[] range = new int[2];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+//             把回文看成中间的部分全是同一字符，左右部分相对称
+//             找到下一个与当前字符不同的字符
+            i = findLongest(str, i, range);
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+    
+    public static int findLongest(char[] str, int low, int[] range) {
+//         查找中间部分
+        int high = low;
+        while (high < str.length - 1 && str[high + 1] == str[low]) {
+            high++;
+        }
+//         定位中间部分的最后一个字符
+        int ans = high;
+//         从中间向左右扩散
+        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+            low--;
+            high++;
+        }
+//         记录最大长度
+        if (high - low > range[1] - range[0]) {
+            range[0] = low;
+            range[1] = high;
+        }
+        return ans;
     }
 }
