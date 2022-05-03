cc685263f308e1c8ab2265e0a1a39c1aa6ca905f
Miao Sun <hfut_sm@163.com>
2022-05-03 23:23:27 +0800

    Create 937. 重新排列日志文件.java

---
+++ LeetCodeEveryDay/937. 重新排列日志文件.java
@@ -0,0 +1,10 @@
+## 第一版 首先要搞清楚题目中的这句话：“字母日志 在内容不同时，忽略标识符后，按内容字母顺序排序；在内容相同时，按标识符排序。”
+
+已完成
+执行用时：0 ms
+输入
+["let1 art ca n","let1 art c an","let1 art can"]
+预期结果
+["let1 art c an","let1 art ca n","let1 art can"]
+
+本体以自定义排序作为考察点，题解见："https://leetcode-cn.com/problems/reorder-data-in-log-files/solution/zhong-xin-pai-lie-ri-zhi-wen-jian-by-lee-egtm/
